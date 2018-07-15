14c62c4b573b6307be0f693009af465fb82e6c0e
JScarlet <x18321758289@outlook.com>
2018-07-15 19:19:04 +0800

    [add]完成了一个单机版本，能够正确保存 [todo]多机同时操作和一些显示细节

--- js/main_page.js
+++ js/main_page.js
@@ -1,6 +1,8 @@
 let docText = [];
 let currentSentenceIndex = 0;
 let currentDocId = 0;
+let unfinishedDocs = [];
+let typeMap = new Map();
 $(document).ready(function () {
     let doc_index = 1;
     $.ajax({
@@ -14,44 +16,57 @@ $(document).ready(function () {
             console.log(xhr)
         },
         success: function(d){
-            let unfinished_docs = d["unfinished_doc_list"];
-            // let doc_index = Math.floor(Math.random() * unfinished_docs.length);
-            let data = {"doc_id": doc_index};
+            unfinishedDocs = d["unfinished_doc_list"];
+            // let doc_index = Math.floor(Math.random() * unfinishedDocs.length);
+            // let data = {"doc_id": doc_index};
             currentDocId = doc_index;
-            $.ajax({
-                async: true,
-                url: "http://10.141.221.75/getDocById/",
-                type: "post",
-                contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(data),
-                error: function (xhr, status, errorThrown) {
-                    console.log("Error " + errorThrown);
-                    console.log("Status: " + status);
-                    console.log(xhr)
-                },
-                success: function(d){
-                    console.log(d);
-                    console.log(d.length);
-                    if(d.length > 0){
-                        for(let i = 0; i < d.length; i++){
-                            let data = {
-                                "sentenceId": "sentence-" + d[i].sentence_index,
-                                "sentence": d[i].text
-                            };
-                            console.log(data);
-                            $("#template").tmpl(data).appendTo(".doc-panel-scroll");
-                            docText.push(d[i].text)
-                        }
-                        $(".sentence-panel-scroll").html(d[0].text);
-                        currentSentenceIndex = 0;
-                        getAnnotationType(d[0].doc_id, d[0].sentence_index)
-                    }
-                }
-            });
+            getDocById(doc_index);
         }
     });
 
 });
+
+function docNext(){
+    if(currentDocId < unfinishedDocs[unfinishedDocs.length - 1]){
+        cleanDocContent();
+        cleanRadioBtns();
+        let temp = currentDocId + 1;
+        while(temp < unfinishedDocs[unfinishedDocs.length - 1] && $.inArray(temp, unfinishedDocs) === -1){
+            temp += 1;
+        }
+        currentDocId = temp;
+        getDocById(currentDocId);
+    }
+
+    if(currentDocId === unfinishedDocs[unfinishedDocs.length - 1]){
+        $("#doc-next").attr("disabled", "true");
+    }
+    if(currentDocId !== 1){
+        $("#doc-previous").removeAttr("disabled");
+    }
+
+}
+
+function docPre(){
+    if(currentDocId > 1){
+        cleanDocContent();
+        cleanRadioBtns();
+        let temp = currentDocId - 1;
+        while(temp > 1 && $.inArray(temp, unfinishedDocs) === -1){
+            temp -= 1;
+        }
+        currentDocId = temp;
+        getDocById(currentDocId);
+    }
+
+    if(currentDocId === 1){
+        $("#doc-previous").attr("disabled", "true");
+    }
+    if(currentDocId !== unfinishedDocs[unfinishedDocs.length - 1]){
+        $("#doc-next").removeAttr("disabled");
+    }
+
+}
 
 function sentenceNext(){
     let length = docText.length;
@@ -110,9 +125,117 @@ function getAnnotationType(doc_id, sentence_index){
     });
 }
 
+function  getDocById(doc_id){
+    let data = {"doc_id": doc_id};
+    $.ajax({
+        async: true,
+        url: "http://10.141.221.75/getDocById/",
+        type: "post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+        error: function (xhr, status, errorThrown) {
+            console.log("Error " + errorThrown);
+            console.log("Status: " + status);
+            console.log(xhr)
+        },
+        success: function(d){
+            console.log(d);
+            console.log(d.length);
+            if(d.length > 0){
+                for(let i = 0; i < d.length; i++){
+                    let data = {
+                        "sentenceId": "sentence-" + d[i].sentence_index,
+                        "sentence": d[i].text
+                    };
+                    console.log(data);
+                    $("#template").tmpl(data).appendTo(".doc-panel-scroll");
+                    docText.push(d[i].text)
+                }
+                $(".sentence-panel-scroll").html(d[0].text);
+                currentSentenceIndex = 0;
+                getAnnotationType(d[0].doc_id, d[0].sentence_index)
+            }
+        }
+    });
+}
+
 function cleanRadioBtns(){
     for(let i = 0; i < 3; i++){
         let radioName = "#optionsRadios" + i;
         $(radioName).prop("checked", false);
     }
+}
+
+function cleanDocContent(){
+    $(".doc-panel-scroll").children().remove();
+}
+
+$(function(){
+    $(":radio").click(function(){
+        // console.log($(this).val());
+        let index = $(this).val().split("-")[1];
+        console.log(index);
+        let key = currentDocId + "-" + currentSentenceIndex;
+        typeMap.set(key, index);
+
+        console.log(typeMap);
+        // console.log(typeof typeMap);
+        // console.log(JSON.stringify(typeMap));
+        console.log(MapToJson(typeMap));
+    });
+});
+
+/**
+ * @return {string}
+ */
+function MapToJson(m) {
+    let str = '{';
+    let i = 1;
+    m.forEach(function (item, key, mapObj) {
+        if (mapObj.size === i) {
+            str += '"' + key + '":"' + item + '"';
+        } else {
+            str += '"' + key + '":"' + item + '",';
+        }
+        i++;
+    });
+    str += '}';
+    //console.log(str);
+    return str;
+}
+
+function transformTypeDataToJson(){
+    let data = [];
+    typeMap.forEach(function (value, key, map) {
+        let docSentenceIdArray = key.split("-");
+        let docId = docSentenceIdArray[0];
+        let sentenceId = docSentenceIdArray[1];
+        let text = docText[sentenceId];
+        let type = value;
+        console.log("docId: " + docId + ", sentenceId: " + sentenceId + ", text: " + text + ", type: " + type);
+        let temp = {"doc_id": docId, "sentence_index": sentenceId, "text": text, "type": type};
+        data.push(temp);
+    });
+    return data;
+}
+
+function saveTypeData(){
+    let data = transformTypeDataToJson();
+    $.ajax({
+        async: true,
+        url: "http://10.141.221.75/saveSentenceAnnotation/",
+        type: "post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+        error: function (xhr, status, errorThrown) {
+            console.log("Error " + errorThrown);
+            console.log("Status: " + status);
+            console.log(xhr)
+        },
+        success: function(d){
+            if(d === "save successful"){
+                alert("save successful");
+            }
+        }
+    });
 }
